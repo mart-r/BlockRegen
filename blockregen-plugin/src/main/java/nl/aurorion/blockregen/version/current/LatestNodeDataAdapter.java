296a7ecadf6f1a6d4f1d79bea1206a83be036d2c
Wertik <jregvx@gmail.com>
2021-01-19 17:50:06 +0100

    Implement LatestNodeDataAdapter.

--- blockregen-plugin/src/main/java/nl/aurorion/blockregen/version/current/LatestNodeDataAdapter.java
+++ blockregen-plugin/src/main/java/nl/aurorion/blockregen/version/current/LatestNodeDataAdapter.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
+import com.google.gson.reflect.TypeToken;
 import nl.aurorion.blockregen.version.api.INodeData;
 import nl.aurorion.blockregen.version.api.INodeDataJsonAdapter;
 
@@ -12,12 +13,23 @@ import java.lang.reflect.Type;
 public class LatestNodeDataAdapter implements INodeDataJsonAdapter {
 
     @Override
-    public INodeData deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+    public INodeData deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+        INodeData data = context.deserialize(json, new TypeToken<LatestNodeData>() {
+        }.getType());
+
+        if (data instanceof LatestNodeData) {
+            return data;
+        }
         return null;
     }
 
     @Override
-    public JsonElement serialize(INodeData nodeData, Type type, JsonSerializationContext jsonSerializationContext) {
-        return null;
+    public JsonElement serialize(INodeData nodeData, Type type, JsonSerializationContext context) {
+
+        if (!(nodeData instanceof LatestNodeData))
+            return null;
+
+        LatestNodeData legacyNodeData = (LatestNodeData) nodeData;
+        return context.serialize(legacyNodeData);
     }
 }
